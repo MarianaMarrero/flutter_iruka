8102f5222d3452514d118a4ed0a412e50c13e7f9
mariana.marrero.contractor <mariana.marrero.contractor@bbva.com>
2020-10-29 10:31:08 -0300

    event list

--- lib/screens/events_list.dart
+++ lib/screens/events_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:poc_iruka_flutter/widgets/appbar.dart';
 import 'package:poc_iruka_flutter/widgets/bottom_navigation_bar.dart';
@@ -21,17 +22,50 @@ class _EventsListState extends State<EventsList> {
       events = arguments['events'];
       print(events);
     }
+
     return Scaffold(
-        appBar: appBar(title),
+        appBar: appBar(title.toUpperCase()),
         drawer: DrawerApp(),
         body: GridView.count(
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 1,
           crossAxisCount: 1,
+          childAspectRatio: (7),
           children: events.map((event) {
             return Container(
-              child: Text(event['description']),
+              child: _buildItem(event['description']),
             );
           }).toList(),
         ),
+        persistentFooterButtons: [
+          _buildRaisedButton("HISTORIAL"),
+          _buildRaisedButton("AGENDAR")
+        ],
         bottomNavigationBar: BottomNavigator());
+  }
+
+  RaisedButton _buildRaisedButton(String textButton) {
+    return new RaisedButton(
+      onPressed: () {},
+      child: new Text(textButton,
+          style: TextStyle(fontSize: 16, color: Colors.white)),
+      shape:
+          RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+      color: Color.fromRGBO(0, 83, 118, 1.0),
+    );
+  }
+
+  Widget _buildItem(String textTitle) {
+    return new ListTile(
+      title: new Text(textTitle),
+      leading: new Container(
+        color: Colors.grey,
+        child: new IconButton(
+            icon: new Icon(Icons.event, color: Colors.white), onPressed: null),
+      ),
+      onTap: () {
+        print(textTitle);
+      },
+    );
   }
 }
